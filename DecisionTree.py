a10ba69e599581afc3a234ccdd8e6f08c5a15f84
AnuVerse-1504 <anushka.s1504@gmail.com>
2026-04-22 20:00:10 +0530

    Added decision tree ml code

---
+++ DecisionTree.py
@@ -0,0 +1,25 @@
+# Import libraries
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset (Iris)
+data = datasets.load_iris()
+X = data.data
+y = data.target
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Create model
+model = DecisionTreeClassifier()
+
+# Train model
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+# Accuracy
+print("Accuracy:", accuracy_score(y_test, y_pred))
