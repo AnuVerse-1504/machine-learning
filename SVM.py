a4339f5cf34b1d7791dab7f1e27985c0f438f56f
AnuVerse-1504 <anushka.s1504@gmail.com>
2026-04-22 19:54:09 +0530

    Added SVM ml code

---
+++ SVM.py
@@ -0,0 +1,26 @@
+# Import libraries
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+# Load dataset (Iris dataset)
+data = datasets.load_iris()
+X = data.data
+y = data.target
+
+# Split into training and testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Create SVM model
+model = SVC(kernel='linear')   # you can use 'rbf', 'poly' also
+
+# Train model
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
